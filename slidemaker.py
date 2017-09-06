a1887235f2d130d07ccb4deceb845714e665507b
Cameron Yee <yee.camero23@gmail.com>
2017-09-06 08:58:11 -0600

    Added initial python script

---
+++ slidemaker.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+from pptx import Presentation
+from pptx.util import Inches
+#import argparse
+
+# Functions go here
+def create_ppt(input, output, lyrics):
+
+if __name__ == "__main__":
+#    args = parse_args()
+    create_ppt(args.infile.name, args.outfile.name, lyrics)
+
+	
